063b09641c0e0c9e7ba038464adabd28bd5cdee8
explicitname <explicit6911@gmail.com>
2015-02-04 11:20:31 -0500

    Moved populating function

--- EnigmaMachineClass.cpp
+++ EnigmaMachineClass.cpp
@@ -2,18 +2,18 @@
 #include <string>
 #include <vector>
 
-std::vector construct_shifts(const std::string &wiring)
+/*std::vector construct_shifts(const std::string &wiring)
 {
 	//Write this function
 	#error Unimplemented
-}
+}*/
 
-char shift_char(char c, int offset)
+/*char shift_char(char c, int offset)
 {
 	//Write this function
 	#error Unimplemented
 
-}
+}*/
 
 int charToNum(char x)
 
@@ -24,7 +24,7 @@ int charToNum(char x)
 		{
 			y = y - 25;
 		}
-		else if ( y < 0)
+	else if ( y < 0)
 		{
 			y = y + 25;
 		}
@@ -42,16 +42,17 @@ char numToChar(int n)
 
 }
 
-/*int offset1(std::string alpha)
+int populateArray(std::string rotor1, std::string alpha, int populatedArray[25])
 {
+	//This needs to be a function
 	for (int i = 0; i < 26; i++)
 	{
-		int x = charToNum(rotor1[i]) - charToNum(alpha[i]);
-		std::cout << array1[i] << std::endl;
+		populatedArray[i] = charToNum(rotor1[i]) - charToNum(alpha[i]);
+		//std::cout << populatedArray[i] << std::endl;
 	}
 
-	return(x);
-}*/
+	return(populatedArray[25]);
+}
 
 
 int main()
@@ -60,64 +61,28 @@ char x;
 //int n;
 std::string rotor1 = "EKMFLGDQVZNTOWYHXUSPAIBRCJ";
 std::string alpha = "ABCDEFGHIJKLMNOPQRSTUVQXYZ";
-int array1[25];
+int populatedArray[25];
 int off = 0;
 int encrypted = 0;
 
-//This needs to be a function
-for (int i = 0; i < 26; i++)
-{
-	array1[i] = charToNum(rotor1[i]) - charToNum(alpha[i]);
-	//std::cout << array1[i] << std::endl;
-}
-
+populatedArray[25] = populateArray(rotor1, alpha, populatedArray);
 std::cout << "Enter a letter to Encrypt: " << std::endl;
 std::cin >> x;
+
 
 for (int j = 0; j < 26; j++)
 {
 	if (x == alpha[j])
 	{
-		off = array1[j];
-		std::cout << off << std::endl;
+		off = populatedArray[j];
+		//std::cout << off << std::endl;
 	}
 }
 
 encrypted = charToNum(x) + off;
-std::cout << encrypted << std::endl;
+//std::cout << encrypted << std::endl;
 
 std::cout << numToChar(encrypted);
 
-
-
-
-
-
-
-/*std::cout << "Enter a letter to convert to a number: ";
-std::cin >> x;
-charToNum(x);
-std::cout << (int)x << std::endl;
-
-std::cout << "Enter a Num to convert to a char: ";
-std::cin >> n;
-numToChar(n);
-std::cout << (char)n << std::endl;*/
-
-
-
-
-
-
-
-
-
-
-
-
-
 	return(0);
 }
-
-
-
