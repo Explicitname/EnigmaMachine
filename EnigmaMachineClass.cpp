4086de4805f7ad65266c0e46bf137bf523e16676
explicitname <explicit6911@gmail.com>
2015-02-04 10:19:14 -0500

    Class work, messy but functional

---
+++ EnigmaMachineClass.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <string>
+
+
+int charToNum(char x)
+
+{
+	int y = (int) x;
+	y = y - (int)'A';
+	if (y > 25)
+		{
+			y = y - 25;
+		}
+		else if ( y < 0)
+		{
+			y = y + 25;
+		}
+	std::cout << y << std::endl;
+	return(y);
+}
+
+char numToChar(int n)
+
+{
+	n = (int)'A' + n;
+	std::cout << n << std::endl;
+	char c = (char) n;
+	return(c);
+
+}
+
+/*int offset1(std::string alpha)
+{
+	for (int i = 0; i < 26; i++)
+	{
+		int x = charToNum(rotor1[i]) - charToNum(alpha[i]);
+		std::cout << array1[i] << std::endl;
+	}
+
+	return(x);
+}*/
+
+
+int main()
+{
+char x;
+//int n;
+std::string rotor1 = "EKMFLGDQVZNTOWYHXUSPAIBRCJ";
+std::string alpha = "ABCDEFGHIJKLMNOPQRSTUVQXYZ";
+int array1[25];
+int off = 0;
+int encrypted = 0;
+
+for (int i = 0; i < 26; i++)
+{
+	array1[i] = charToNum(rotor1[i]) - charToNum(alpha[i]);
+	//std::cout << array1[i] << std::endl;
+}
+
+std::cout << "Enter a letter to Encrypt: " << std::endl;
+std::cin >> x;
+
+for (int j = 0; j < 26; j++)
+{
+	if (x == alpha[j])
+	{
+		off = array1[j];
+		std::cout << off << std::endl;
+	}
+}
+
+encrypted = charToNum(x) + off;
+std::cout << encrypted << std::endl;
+
+std::cout << numToChar(encrypted);
+
+
+
+
+
+
+
+/*std::cout << "Enter a letter to convert to a number: ";
+std::cin >> x;
+charToNum(x);
+std::cout << (int)x << std::endl;
+
+std::cout << "Enter a Num to convert to a char: ";
+std::cin >> n;
+numToChar(n);
+std::cout << (char)n << std::endl;*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return(0);
+}
+
+
+
