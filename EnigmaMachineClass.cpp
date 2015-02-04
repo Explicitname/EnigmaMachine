36a4a4d48efb3c2076e4d0e8b84c4be19a3f0fe8
explicitname <explicit6911@gmail.com>
2015-02-04 12:20:30 -0500

    Added shift_char, also added some comments for review

--- EnigmaMachineClass.cpp
+++ EnigmaMachineClass.cpp
@@ -2,20 +2,15 @@
 #include <string>
 #include <vector>
 
-/*std::vector construct_shifts(const std::string &wiring)
+std::vector construct_shifts(const std::string &wiring)//Unsure of what to put here.
 {
 	//Write this function
 	#error Unimplemented
-}*/
+}
 
-/*char shift_char(char c, int offset)
-{
-	//Write this function
-	#error Unimplemented
 
-}*/
 
-int charToNum(char x)
+int charToNum(char x)//Converts a capital A-Z into a number from 0-25 and implements wrap around
 
 {
 	int y = (int) x;
@@ -28,21 +23,21 @@ int charToNum(char x)
 		{
 			y = y + 25;
 		}
-	std::cout << y << std::endl;
+	//std::cout << y << std::endl;
 	return(y);
 }
 
-char numToChar(int n)
+char numToChar(int n)//Converts a number, from 0-25, to a capital letter A-Z
 
 {
 	n = (int)'A' + n;
-	std::cout << n << std::endl;
+	//std::cout << n << std::endl;
 	char c = (char) n;
 	return(c);
 
 }
 
-int populateArray(std::string rotor1, std::string alpha, int populatedArray[25])
+int populateArray(std::string rotor1, std::string alpha, int populatedArray[25])//Populates the array with offset values, based on rotator 1
 {
 	//This needs to be a function
 	for (int i = 0; i < 26; i++)
@@ -54,18 +49,27 @@ int populateArray(std::string rotor1, std::string alpha, int populatedArray[25])
 	return(populatedArray[25]);
 }
 
+char shift_char(char c, int offset)//Encrypts the input char, A-Z, and returns the encrypted char, A-Z
+{
+	int EncryptedNum;
+	char EncryptedChar;
+	EncryptedNum = charToNum(c) + offset;
+	EncryptedChar = numToChar(EncryptedNum);
+	return(EncryptedChar);
+}
+
 
 int main()
 {
 char x;
-//int n;
 std::string rotor1 = "EKMFLGDQVZNTOWYHXUSPAIBRCJ";
 std::string alpha = "ABCDEFGHIJKLMNOPQRSTUVQXYZ";
 int populatedArray[25];
-int off = 0;
-int encrypted = 0;
+int offset = 0;
 
-populatedArray[25] = populateArray(rotor1, alpha, populatedArray);
+
+populatedArray[25] = populateArray(rotor1, alpha, populatedArray);//Seems like this should go somewhere else
+
 std::cout << "Enter a letter to Encrypt: " << std::endl;
 std::cin >> x;
 
@@ -74,15 +78,9 @@ for (int j = 0; j < 26; j++)
 {
 	if (x == alpha[j])
 	{
-		off = populatedArray[j];
-		//std::cout << off << std::endl;
+		offset = populatedArray[j];
 	}
 }
-
-encrypted = charToNum(x) + off;
-//std::cout << encrypted << std::endl;
-
-std::cout << numToChar(encrypted);
-
-	return(0);
+std::cout << shift_char(x, offset);
+return(0);
 }
